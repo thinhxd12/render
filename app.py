dab93344b6c1d0c92439053d132e750ddab816a4
thinhxd12 <thinhxd12@gmail.com>
2026-05-14 20:29:35 +0700

    333

--- app.py
+++ app.py
@@ -109,8 +109,8 @@ async def scrape_data(request: ScrapeRequest, api_key: str = Security(api_key_he
 
     try:
         page.route("**/*", intercept_route)
-        await page.goto(target_url, wait_until="domcontentloaded", timeout=30000)
-        await page.wait_for_load_state("networkidle", timeout=30000)
+        await page.goto(target_url, wait_until="domcontentloaded")
+        # await page.wait_for_load_state("networkidle", timeout=30000)
         
         raw_html = await page.content()
         return {"success": True, "target": target_url, "html": raw_html}
